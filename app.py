a27184a4545651304b10c7bbd4c6639240ea8191
Ragon Géraldine <gydee72@gmail.com>
2026-08-23 20:54:43 +0000

    Ajout du template HTML accueil

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def accueil():
-    return "<h1>Bienvenue sur mon site depuis Codespace !</h1>"
+    return render_template("accueil.html", nom="Visiteur")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
